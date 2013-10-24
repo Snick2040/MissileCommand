bb97a04df0ce0954c2d4cc9e9c6fd5222dc28a6c
Nick <nick@Nicks-MacBook-Pro.local>
2013-10-24 19:49:11 -0400

    Initial Commit

---
+++ MissileCommand/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MissileCommand
+//
+//  Created by Nick on 10/24/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MissileCommand/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MissileCommand
+//
+//  Created by Nick on 10/24/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
